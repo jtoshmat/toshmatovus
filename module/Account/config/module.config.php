15ad4fb1c74c9d86101a43784595d69dc77fec06
Jon Toshmatov <jontoshmatov@yahoo.com>
2015-02-28 13:54:37 -0500

    Account and Candidate module created

---
+++ module/Account/config/module.config.php
@@ -0,0 +1,136 @@
+<?php
+	return array(
+		'controllers' => array(
+			'invokables' => array(
+				'Account\Controller\Index' => 'Account\Controller\IndexController',
+			),
+		),
+		'router' => array(
+			'routes' => array(
+				'account' => array(
+					'type' => 'Zend\Mvc\Router\Http\Literal',
+					'options' => array(
+						'route'    => '/account',
+						'defaults' => array(
+							'controller' => 'Account\Controller\Index',
+							'action'     => 'index',
+						),
+					),
+				),
+
+				'test' => array(
+					'type' => 'Zend\Mvc\Router\Http\Segment',
+					'options' => array(
+						'route'    => '/test[/:action]',
+						'constraints' => array(
+							'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+
+						),
+						'defaults' => array(
+							'controller' => 'Account\Controller\Index',
+							'action'     => 'test',
+						),
+					),
+				),
+
+
+				// The following is a route to simplify getting started creating
+				// new controllers and actions without needing to create a new
+				// module. Simply drop new controllers in, and you can access them
+				// using the path /account/:controller/:action
+				'account' => array(
+					'type'    => 'Literal',
+					'options' => array(
+						'route'    => '/account',
+						'defaults' => array(
+							'__NAMESPACE__' => 'Account\Controller',
+							'controller'    => 'Index',
+							'action'        => 'index',
+						),
+					),
+					'may_terminate' => true,
+					'child_routes' => array(
+						'default' => array(
+							'type'    => 'Segment',
+							'options' => array(
+								'route'    => '/[:controller[/:action]]',
+								'constraints' => array(
+									'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+									'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+								),
+								'defaults' => array(
+								),
+							),
+						),
+
+						'test2' => array(
+							'type' => 'Zend\Mvc\Router\Http\Segment',
+							'options' => array(
+								'route'    => 'development[/:action]',
+								'constraints' => array(
+									'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+
+								),
+								'defaults' => array(
+									'controller' => 'Account\Controller\Index',
+									'action'     => 'test',
+								),
+							),
+						),
+
+
+					),
+				),
+			),
+		),
+		'service_manager' => array(
+			'abstract_factories' => array(
+				'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+				'Zend\Log\LoggerAbstractServiceFactory',
+			),
+			'factories' => array(
+				'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+			),
+			'aliases' => array(
+				'translator' => 'MvcTranslator',
+			),
+		),
+		'translator' => array(
+			'locale' => 'en_US',
+			'translation_file_patterns' => array(
+				array(
+					'type'     => 'gettext',
+					'base_dir' => __DIR__ . '/../language',
+					'pattern'  => '%s.mo',
+				),
+			),
+		),
+		'controllers' => array(
+			'invokables' => array(
+				'Account\Controller\Index' => 'Account\Controller\IndexController'
+			),
+		),
+		'view_manager' => array(
+			'display_not_found_reason' => true,
+			'display_exceptions'       => true,
+			'doctype'                  => 'HTML5',
+			'not_found_template'       => 'error/404',
+			'exception_template'       => 'error/index',
+			'template_map' => array(
+				'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+				'account/index/index' => __DIR__ . '/../view/account/index/index.phtml',
+				'error/404'               => __DIR__ . '/../view/error/404.phtml',
+				'error/index'             => __DIR__ . '/../view/error/index.phtml',
+			),
+			'template_path_stack' => array(
+				__DIR__ . '/../view',
+			),
+		),
+		// Placeholder for console routes
+		'console' => array(
+			'router' => array(
+				'routes' => array(
+				),
+			),
+		),
+	);
---
+++ module/Account/src/Account/Controller/IndexController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonAccount for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Account\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+
+        return new ViewModel();
+    }
+
+	public function interviewAction()
+	{
+
+
+		return new ViewModel();
+	}
+	public function testAction()
+	{
+
+
+
+		return new ViewModel();
+	}
+}
